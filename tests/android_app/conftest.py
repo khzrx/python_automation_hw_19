4a8fc287796178fd6bacfed96471bbf5a84ff452
Filipp Goncharenko <phil.can@yandex.ru>
2025-08-09 15:36:49 +0300

    Добавлен config файл. Изменен conftest. Добавлен тест на онбординг-скрин.

--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -1,57 +1,46 @@
-import allure
-import allure_commons
 import pytest
-from appium.options.android import UiAutomator2Options
 from appium import webdriver
-from selene import browser, support
-import os
+from selene import browser
 import utils
 from dotenv import load_dotenv
+import config
+
+def pytest_addoption(parser):
+
+    parser.addoption(
+        "--context",
+        default="bstack",
+        help="Specify the test context"
+    )
 
 
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
+def pytest_configure(config):
+    context = config.getoption("--context")
+    env_file_path = f".env.{context}"
+
+    load_dotenv(dotenv_path=env_file_path)
+
+
+@pytest.fixture
+def context(request):
+    return request.config.getoption("--context")
+
 
 @pytest.fixture(scope='function', autouse=True)
-def mobile_management():
-    options = UiAutomator2Options().load_capabilities({
-        'platformVersion': os.getenv('bstack_platformVersion'),
-        'deviceName': os.getenv('bstack_deviceName'),
+def mobile_management(context):
+    options = config.to_driver_options(context=context)
 
-        'app': os.getenv('bstack_app'),
-
-        'bstack:options': {
-            'projectName': 'QA.GURU homework 19',
-            'buildName': 'browserstack-build-1',
-            'sessionName': 'BStack first_test',
-
-            'userName': os.getenv('bstack_userName'),
-            'accessKey': os.getenv('bstack_accessKey')
-        }
-    })
-
-    with allure.step('Init app session'):
-        browser.config.driver = webdriver.Remote(
-            'http://hub.browserstack.com/wd/hub',
-            options=options
-        )
-
-    browser.config.timeout = float(os.getenv('timeout', '10.0'))
-
-    browser.config._wait_decorator = support._logging.wait_with(
-        context=allure_commons._allure.StepContext
-    )
+    browser.config.driver = webdriver.Remote(options.get_capability('remote_url'), options=options)
+    browser.config.timeout = 10.0
 
     yield
 
     utils.allure.attach_screenshot()
     utils.allure.attach_xml()
-
     session_id = browser.driver.session_id
 
-    with allure.step('Teardown app session'):
-        browser.quit()
+    browser.quit()
 
-    utils.allure.attach_bstack_video(session_id)
+    if context == 'bstack':
+        utils.allure.attach_bstack_video(session_id)
 
--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -1,22 +1,29 @@
 import allure
 from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, have
+from selene import browser, have, be
 
 
 def test_search():
+    with allure.step('Skip onboarding'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+
     with allure.step('Type search'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
 
     with allure.step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
         results.first.should(have.text('Appium'))
 
+
 def test_search_and_click():
+    with allure.step('Skip onboarding'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+
     with allure.step('Type search'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
 
     with allure.step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
@@ -25,3 +32,60 @@ def test_search_and_click():
 
     with allure.step('Click on content'):
         results.first.click()
+
+
+def test_onboarding_texts():
+    with allure.step('Verify text on 1st page of onboarding screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')
+                        ).should(have.text('The Free Encyclopedia\n…in over 300 languages'))
+
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/secondaryTextView')
+                        ).should(have.text('We’ve found the following on your device:'))
+
+    with allure.step('Click "Continue" button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+    with allure.step('Verify text on 2nd page of onboarding screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')
+                        ).should(have.text('New ways to explore'))
+
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/secondaryTextView')
+                        ).should(have.text(
+            'Dive down the Wikipedia rabbit hole with a constantly updating Explore feed. \n'
+            'Customize the feed to your interests – whether it’s learning about historical events '
+            'On this day, or rolling the dice with Random.'))
+
+    with allure.step('Click "Continue" button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+    with allure.step('Verify text on 3rd page of onboarding screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')
+                        ).should(have.text('Reading lists with sync'))
+
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/secondaryTextView')
+                        ).should(have.text(
+            'You can make reading lists from articles you want to read later, even when you’re offline. \n'
+            'Login to your Wikipedia account to sync your reading lists. Join Wikipedia'))
+
+    with allure.step('Click "Continue" button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_forward_button')).click()
+
+    with allure.step('Verify text on 3rd page of onboarding screen'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/primaryTextView')
+                        ).should(have.text('Data & Privacy'))
+
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/secondaryTextView')
+                        ).should(have.text(
+            'We believe that you should not have to provide personal information to participate in the free knowledge '
+            'movement. Usage data collected for this app is anonymous. '
+            'Learn more about our privacy policy and terms of use.'))
+
+    with allure.step('Verify button has text "Get Started"'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_done_button')
+                        ).should(have.text('Get started'))
+
+    with allure.step('Click "Get started" button'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_done_button')).click()
+
+    with allure.step('Verify search field is visible'):
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_container')).should(be.visible)
